61f7bf7cd497e1a43de6f4fa4117bff81936ac2e
zhangyx_xyz <zhangyx_xyz@outlook.com>
2017-08-14 22:50:21 +0800

    AssetBundleEditor初次提交

---
+++ Assets/Scripts/ExtraTools/AssetUtil.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// 加载进度
+/// </summary>
+public delegate void LoadProgress(string bundleName, float progress);
+
+/// <summary>
+/// 加载完成时候的调用
+/// </summary>
+public delegate void LoadComplete(string bundleName);
+
+/// <summary>
+/// 加载assetbundle的回调
+/// </summary>
+public delegate void LoadAssetBundleCallback(string sceneName, string bundleName);
+
+public class AssetUtil
+{
+
+}
